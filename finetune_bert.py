9f5cb81b56842b53b34c4de6d8e8105277a52b5a
Grace Lam <gracelam@mit.edu>
2021-03-02 15:53:58 -0800

    Add bert evaluation code

--- finetune_bert.py
+++ finetune_bert.py
@@ -7,8 +7,8 @@ from transformers import AutoModelForMaskedLM
 from transformers import DataCollatorForLanguageModeling
 from transformers import Trainer, TrainingArguments
 
-train_file = 'data/80_10_10/train_textbook_data.txt'
-eval_file = 'data/80_10_10/dev_textbook_data.txt'
+train_file = 'bert_data/80_10_10/train_textbook_data.txt'
+eval_file = 'bert_data/80_10_10/dev_textbook_data.txt'
 model_checkpoint = 'bert-base-uncased'
 model_dir='bert_mlm/80_10_10/'
 block_size = 128
