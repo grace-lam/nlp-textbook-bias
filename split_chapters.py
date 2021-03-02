0c15e4ccf2860532a86c711318b0a0661bf7ad94
Grace Lam <gracelam@mit.edu>
2021-03-01 19:12:38 -0800

    Modify formatting of chapter split

--- split_chapters.py
+++ split_chapters.py
@@ -7,12 +7,12 @@ original_directory = 'textbook_data/'
 for file in os.listdir(directory):
      filename = os.fsdecode(file)
      if filename.endswith(".txt"):
-         textbook_name = filename[:-3]
+         textbook_name = filename[:-4]
          with open(original_directory + filename, 'r') as textbook_reader:
              os.makedirs(chapters_directory + textbook_name, exist_ok=True)
              next_chapter = 2
              for sentence in textbook_reader:
                  if "chapter " + str(next_chapter) in sentence.lower():
                      next_chapter += 1
-                 with open(chapters_directory + textbook_name + "/chapter-" + str(next_chapter - 1), 'a') as textbook_writer:
+                 with open(chapters_directory + textbook_name + "/chapter_" + str(next_chapter - 1) + ".txt", 'a') as textbook_writer:
                      textbook_writer.write(sentence)
