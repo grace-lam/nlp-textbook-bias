da15520938c2323c06bb03e9e709511cb33b306f
Marilyn Zhang <marilyn.zhang1230@gmail.com>
2021-03-01 18:17:04 -0800

    split textbooks by chapter

---
+++ split_chapters.py
@@ -0,0 +1,18 @@
+import os
+
+directory = os.fsencode('textbook_data/')
+chapters_directory = 'textbook_data_chapters/'
+original_directory = 'textbook_data/'
+
+for file in os.listdir(directory):
+     filename = os.fsdecode(file)
+     if filename.endswith(".txt"):
+         textbook_name = filename[:-3]
+         with open(original_directory + filename, 'r') as textbook_reader:
+             os.makedirs(chapters_directory + textbook_name, exist_ok=True)
+             next_chapter = 2
+             for sentence in textbook_reader:
+                 if "chapter " + str(next_chapter) in sentence.lower():
+                     next_chapter += 1
+                 with open(chapters_directory + textbook_name + "/chapter-" + str(next_chapter - 1), 'a') as textbook_writer:
+                     textbook_writer.write(sentence)
